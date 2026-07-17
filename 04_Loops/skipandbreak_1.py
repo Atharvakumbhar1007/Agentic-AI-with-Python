9ccac1b1954c8c4aca129a14717f0daff491ca74
Atharva <atharvakumbhar631@gmail.com>
2026-07-17 13:53:46 +0530

    skip and break

---
+++ 04_Loops/skipandbreak_1.py
@@ -0,0 +1,10 @@
+flavours = ["Ginger", "Out of Stock", "Discontinued", "Tulsi"]
+
+for flavour in flavours:
+    if flavour == "Out of stack":
+        continue
+    if flavour == "Discontinued":
+        break
+    print("Discontinued item found")
+    
+print(f"Out side of loop")
