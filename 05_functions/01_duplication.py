efb3538eb7707646f239043d648a93df1bfa1e93
Atharva <atharvakumbhar631@gmail.com>
2026-07-20 12:22:34 +0530

    duplications

---
+++ 05_functions/01_duplication.py
@@ -0,0 +1,7 @@
+def print_order(name, chai_type): #()Inside this brackets there are parameters
+    print(f"{name} ordered {chai_type} chai")
+    
+
+print_order("Om", "Masala")
+print_order("Raj","Ginger")
+print("Atharva","Tulsi")
