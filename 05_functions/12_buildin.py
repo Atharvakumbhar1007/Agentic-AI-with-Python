b58d71c4ba2a3c8e915aa8f5c2e5063311dfa7b3
Atharva <atharvakumbhar631@gmail.com>
2026-07-21 23:12:46 +0530

    build in types

--- 05_functions/12_buildin.py
+++ 05_functions/12_buildin.py
@@ -5,3 +5,16 @@ def chai_flavor(flavor="masala"):
 
 print(chai_flavor.__doc__)
 print(chai_flavor.__name__)
+
+help(len)
+
+def generate_bill(chai=0, samosa=0):
+    """
+    Calculate the total bill for chai and samosa 
+    
+    :param chai: Number of chai cups (10 rupees each)
+    :param samosa: Number of samosa (10 rupees each)
+    :return: (total amount, thank you message)
+    """
+    total = chai*10 + samosa*15
+    return total, "thank you for visiting chaicode.com"
