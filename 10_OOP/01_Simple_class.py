e0464be76b7363c9616394107d31fe3b559c0f55
Atharva <atharvakumbhar631@gmail.com>
2026-07-23 22:33:28 +0530

    oop

--- 10_OOP/01_Simple_class.py
+++ 10_OOP/01_Simple_class.py
@@ -1,5 +1,13 @@
-class Chai:
+class Chai: #class created
+    pass
+
+class ChaiTime:
     pass
 
 print(type(Chai))
+
+ginger_tea = Chai() #oblect created
+print(type(ginger_tea))
+print(type(ginger_tea) is Chai)
+print(type(ginger_tea) is ChaiTime)
 
