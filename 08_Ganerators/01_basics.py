ba31b8c8e58c5caececd4d7d89be4bc043a8bccb
Atharva <atharvakumbhar631@gmail.com>
2026-07-23 14:02:13 +0530

    basics

---
+++ 08_Ganerators/01_basics.py
@@ -0,0 +1,10 @@
+def serve_chai():
+    yield "Cup 1: Masala Chai"
+    yield "Cup 2: Ginger Chai"
+    yield "Cup 3: Elaichi Chai"
+    
+stall = serve_chai()
+
+for cup in stall:
+    print(cup)
+    
