aa6c6b02bd85c4d80ab572bfed00017c0c16bbfc
Atharva <atharvakumbhar631@gmail.com>
2026-07-23 14:49:40 +0530

    basics in python

---
+++ 09_Decorators/basics.py
@@ -0,0 +1,11 @@
+def my_decorator(func):
+    def wrapper():
+        print("Before function runs")
+        func()
+        print("After function runs")
+    return wrapper
+
+@my_decorator
+def greet():
+    print("Hello from decoratord class from chaicode")
+greet()
