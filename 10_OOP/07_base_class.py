f2500bd35272625bf5b23bf0cac6bbe0aae4d4b2
Atharva <atharvakumbhar631@gmail.com>
2026-07-24 23:21:13 +0530

    Update 07_base_class.py

--- 10_OOP/07_base_class.py
+++ 10_OOP/07_base_class.py
@@ -3,9 +3,19 @@ class Chai:
         self.type = type_
         self.strength = strength
         
+# class GingerChai(Chai):
+#     def __init__(self, type_, strength, spice_level):
+#         self.type = type_
+#         self.strength = strength
+#         self.spice_level = spice_level
+        
+# class GingerChai(Chai):
+#     def __init__(self, type_, strength, spice_level):
+#         Chai.__init__(self, type_, strength)
+#         self.spice_level spice_level
+
 class GingerChai(Chai):
-    def __init__(self, type_, strength, spice_level):
-        self.type = type_
-        self.strength = strength
+    def __init__(self, type_, strength):
+        super().__init__(type_, strength)
         self.spice_level = spice_level
         
