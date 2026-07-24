a4aab86bcedc9aa261acd4d579bd86672154eabf
Atharva <atharvakumbhar631@gmail.com>
2026-07-24 23:26:00 +0530

    MRO

---
+++ 10_OOP/MRO.py
@@ -0,0 +1,16 @@
+#MRO = Method Resolution Order
+
+class A:
+    label = "A: Base Class"
+    
+class B:
+    label = "B: Masala Blend"
+    
+class C(A):
+    label = "C: Herbal Blend"
+    
+class D(B, C):
+    pass
+
+cup = D()
+print(cup.label)
