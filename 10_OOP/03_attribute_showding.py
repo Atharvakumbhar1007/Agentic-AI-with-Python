ddf751640bfcf5d1fb26cdc134031a6354fae61d
Atharva <atharvakumbhar631@gmail.com>
2026-07-24 15:56:58 +0530

    Update 03_attribute_showding.py

---
+++ 10_OOP/03_attribute_showding.py
@@ -0,0 +1,15 @@
+class Chai:
+    temperature = "hot"
+    strength = "Strong"
+    
+cutting = Chai()
+print(cutting.temperature)
+
+cutting.temperature = "Mild"
+cutting.cup = "small"
+print("After changing ",cutting.temperature)
+print("cup size is ",cutting.cup)
+print("Direct look into the class ",Chai.temperature)
+
+del cutting.temperature
+print(cutting.temperature)
