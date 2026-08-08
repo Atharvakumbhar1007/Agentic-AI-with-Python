b1b0ce2f2472d90e1af911dab9498d0b95a69d9b
Atharva <atharvakumbhar631@gmail.com>
2026-08-08 13:39:59 +0530

    threads in python

--- 13_Asyncio/03_async.py
+++ 13_Asyncio/03_async.py
@@ -13,3 +13,27 @@ async def main():
         await asyncio.gather(*tasks)
 
 asyncio.run(main())
+
+'''
+aiohttp is an asynchronous HTTP client/server 
+library for Python.
+
+Why use ClientSession?
+=>Instead of creating a new HTTP connection for 
+every request, ClientSession:
+
+Reuses TCP connections.
+Improves performance.
+Reduces latency.
+Is the recommended way to make multiple HTTP 
+requests.
+
+Why use asyncio.gather()?
+
+asyncio.gather() runs multiple coroutines concurrently 
+and waits for all of them to finish, returning 
+their results in the order they were passed.
+
+(*tasks) = > This creates an event loop and runs 
+the main() coroutine
+'''
